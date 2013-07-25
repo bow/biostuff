90b01af524e6c431d07a53bfb1ad80de77c67a4a
bow <bow@bow.web.id>
2013-07-25 11:33:00 +0200

    Update docs for split_bam_by_ref script

--- split_bam_by_ref.py
+++ split_bam_by_ref.py
@@ -1,11 +1,32 @@
-#!/usr/bin/env python
-#
-# split_bam_by_ref.py
-#
-# Given a BAM file, split into multiple files each containing reads mapped to
-# the same reference sequence.
-#
-# (c) 2013 Wibowo Arindrarto [SASC - LUMC]
+#!/usr/bin/env python2
+
+"""
+Spliting a single BAM file into multiple BAM files based on the reference.
+
+
+For a given BAM file, this script will produce multiple BAM files, each
+containing reads mapped to a single record in the BAM reference. For example, a
+BAM file from a full human genome sequencing will be split into at least 24-25
+files (autosomes, sex chromosomes, mitochondrial genome).
+
+By default, the script writes all split BAMs into the current directory.
+For unmapped reads, you may choose whether to output them into a separate file
+or not.
+
+The splitting may be sped up by indexing the BAM file beforehand using samtools
+index. The index must be located in the same directory as the input BAM file,
+with the '.bam.bai' extension. Note that this will not allow you to output
+unmapped BAM reads into its own file, however.
+
+Requirements:
+    * Python == 2.7.x
+    * pysam >= 0.7 <http://www.cgat.org/~andreas/documentation/pysam/api.html>
+
+Copyright (c) 2013 Wibowo Arindrarto <w.arindrarto@lumc.nl>
+Copyright (c) 2013 LUMC Sequencing Analysis Support Core <sasc@lumc.nl>
+MIT License <http://opensource.org/licenses/MIT>
+
+"""
 
 import argparse
 import itertools
@@ -15,20 +36,22 @@ import warnings
 import pysam
 
 
-def split_by_fetch(input_bam, out_dir, index_bam=None):
+def split_by_fetch(input_bam, out_dir):
     """Splits a BAM file into multiple BAM files each with reads mapped to the
-    same reference sequence.
+    same reference sequence by fetching the reads.
 
-    Assumes that the BAM file is indexed.
+    Requires that the BAM file is indexed.
 
-    Args:
-        input_bam -- path to input BAM file
-        out_dir -- directory to write the output BAM files
+    :param input_bam: path to input BAM file
+    :type input_bam: str
+    :param out_dir: path to output directory containing all split BAMs
+    :type out_dir: str
+    :returns: None
 
     """
-    if index_bam is None:
-        index_bam = input_bam + '.bai'
-    assert os.path.exists(index_bam), "BAM index file %r not found." % index_bam
+    input_bam_index = input_bam + '.bai'
+    assert os.path.exists(input_bam_index), "BAM index file %r not found." \
+            % input_bam_index
 
     in_bam = pysam.Samfile(input_bam, 'rb')
 
@@ -52,12 +75,15 @@ def split_by_fetch(input_bam, out_dir, index_bam=None):
 
 def split_by_iter(input_bam, out_dir, unmapped_bam=None):
     """Splits a BAM file into multiple BAM files each with reads mapped to the
-    same reference sequence.
+    same reference sequence by iterating over all reads.
 
-    Args:
-        input_bam -- path to input BAM file
-        out_dir -- directory to write the output BAM files
-        unmapped_bam -- path to output unmapped reads from the input BAM file.
+    :param input_bam: path to input BAM file
+    :type input_bam: str
+    :param out_dir: path to output directory containing all split BAMs
+    :type out_dir: str
+    :param unmapped_bam: file name of BAM file for unmapped reads
+    :type unmapped_bam: str
+    :returns: None
 
     """
     in_bam = pysam.Samfile(input_bam, 'rb')
@@ -104,16 +130,21 @@ def split_by_iter(input_bam, out_dir, unmapped_bam=None):
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser()
+    usage = __doc__.split('\n\n\n')
+    parser = argparse.ArgumentParser(
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            description=usage[0], epilog=usage[1])
+
     parser.add_argument('input_bam', type=str,
             help='Input BAM file')
-    parser.add_argument('-i', '--index', dest='index',
-            default=None, help='Input BAM file index')
-    parser.add_argument('-u', '--unmapped-bam', dest='unmapped_bam',
-            default=None, help='Path to write unmapped BAM records')
     parser.add_argument('-d', '--output-directory', dest='out_dir',
             type=str, default=os.getcwd(),
             help='Output directory')
+    parser.add_argument('-i', '--index', dest='index',
+            default=False, action='store_true',
+            help='Whether to use BAM index or not')
+    parser.add_argument('-u', '--unmapped-bam', dest='unmapped_bam',
+            default=None, help='Path to write unmapped BAM records')
 
     args = parser.parse_args()
 
